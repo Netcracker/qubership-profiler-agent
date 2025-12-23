7a08ce88e60af78b0d2e54bfe17c9d6148865c8b
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2025-12-23 17:05:16 +0300

    feat: deprecate topdump
    fixes https://github.com/Netcracker/qubership-profiler-agent/issues/423

--- diagtools/actions/topdump.go
+++ diagtools/actions/topdump.go
@@ -2,9 +2,7 @@ package actions
 
 import (
 	"context"
-	"time"
 
-	"github.com/Netcracker/qubership-profiler-agent/diagtools/constants"
 	"github.com/Netcracker/qubership-profiler-agent/diagtools/log"
 )
 
@@ -13,53 +11,11 @@ type JavaTopAction struct {
 }
 
 func CreateTopAction(ctx context.Context) (action JavaTopAction, err error) {
-	action = JavaTopAction{
-		Action: Action{
-			DcdEnabled: constants.IsDcdEnabled(),
-			DumpPath:   constants.DumpFolder(),
-			PidName:    "java",
-			Command:    "top",
-			CmdArgs:    []string{"-Hb", "-p{{.Pid}}", "-oTIME+", "-d60", "-n1"},
-			CmdTimeout: 10 * time.Second,
-		},
-	}
-
-	err = action.GetPodName(ctx)
-	if err != nil {
-		return
-	}
-	action.Pid, err = action.GetPid(ctx)
-	if err != nil {
-		return
-	}
-
+	action = JavaTopAction{}
 	return action, nil
 }
 
 func (action *JavaTopAction) GetTop(ctx context.Context) (err error) {
-	err = action.GetDumpFile(constants.TopDumpSuffix)
-	if err != nil {
-		return
-	}
-
-	err = action.GetParams()
-	if err != nil {
-		return
-	}
-
-	log.Infof(ctx, "collecting CPU usage for PID #%v to %s", action.Pid, action.DumpPath)
-	var output []byte
-	output, err = action.RunJCmdWithOutput(ctx)
-	if err != nil {
-		return
-	}
-	log.Infof(ctx, "top dump taken, size in bytes: %d", len(output))
-
-	if action.DcdEnabled && len(output) > 0 {
-		err = action.GetTargetUrl(ctx)
-		if err == nil {
-			err = action.UploadOutputToDiagnosticCenter(ctx, output)
-		}
-	}
-	return
+	log.Infof(ctx, "topdump command is deprecated and has been disabled - skipping execution")
+	return nil
 }
