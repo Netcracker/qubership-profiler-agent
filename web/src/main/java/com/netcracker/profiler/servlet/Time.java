04308c9cef91688cd14e750ad4a0de66e78f288b
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2026-02-25 13:39:06 +0300

    fix: time servlet should register as /time rather than /time/*
    If we register it as /time/*, then /time responds with 404

--- web/src/main/java/com/netcracker/profiler/servlet/Time.java
+++ web/src/main/java/com/netcracker/profiler/servlet/Time.java
@@ -2,11 +2,13 @@ package com.netcracker.profiler.servlet;
 
 import java.io.IOException;
 
+import jakarta.inject.Singleton;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Singleton
 public class Time extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/plain");
