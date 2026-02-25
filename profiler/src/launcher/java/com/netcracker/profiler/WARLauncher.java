85c2c92d74bc070b2fd5d3b4335eed376077aba2
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2026-02-25 13:37:38 +0300

    feat: avoid starting browser in headless mode
    This helps to skip "open browser with Profiler UI" when launching backend in tests

--- profiler/src/launcher/java/com/netcracker/profiler/WARLauncher.java
+++ profiler/src/launcher/java/com/netcracker/profiler/WARLauncher.java
@@ -210,14 +210,18 @@ public class WARLauncher {
             };
 
             URI uri = new URI("http://" + hostName + ":" + port);
-            try {
-                logger.info("Trying to open {} in default browser", uri.toString());
-                if(openWindowsNativeFailed(uri)){
-                    Class.forName("java.awt.Desktop");
-                    openBrowser(uri);
+            if (GraphicsEnvironment.isHeadless()) {
+                logger.info("Running in headless mode, please navigate your browser to {}", uri.toString());
+            } else {
+                try {
+                    logger.info("Trying to open {} in default browser", uri.toString());
+                    if(openWindowsNativeFailed(uri)){
+                        Class.forName("java.awt.Desktop");
+                        openBrowser(uri);
+                    }
+                } catch (ClassNotFoundException e) {
+                    logger.warn("Please, navigate your browser to {}", uri.toString());
                 }
-            } catch (ClassNotFoundException e) {
-                logger.warn("Please, navigate your browser to {}", uri.toString());
             }
 
             tomcat.getServer().await();
